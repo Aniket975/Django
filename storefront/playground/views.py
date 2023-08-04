bd1d14244687870ba1d5bc5ea4250fe535fef13c
Aniket Kumar <ket56790@gmail.com>
2023-08-04 21:25:53 +0000

    5th august

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import OrderItem
+from django.db.models import Q, F
+from store.models import Product
 
 
 def say_hello(request):
-    query_set = OrderItem.objects.filter(product__collection__id=3)
+    # query_set = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
+    # query_set = Product.objects.filter(inventory__lt=10).filter(unit_price__lt=20)
+    # query_set = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    # query_set = Product.objects.filter(Q(inventory__lt=10) | ~Q(unit_price__lt=20))
+    # query_set = Product.objects.filter(inventory = F('unit_price'))
+    query_set = Product.objects.filter(inventory = F('collection__id'))
 
     return render(request, 'hello.html', {'name': 'Aniket', 'products': list(query_set)})
